263b3c215321008b37e7f915a3f1e62fa52b58a5
finest08 <mr.mitcholl@gmail.com>
2022-05-13 16:00:56 +1200

    added person to proto

--- main.go
+++ main.go
@@ -7,10 +7,18 @@ import (
 	"log"
 	"time"
 
+	pb "github.com/finest08/PubSubPublisher/gen/proto/go/proto/greeting/v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	pb "github.com/finest08/PubSubPublisher/gen/proto/go/proto/greeting/v1"
 )
+
+type Person struct {
+	Name string
+	Occupation string
+	Age  string
+
+}
+
 
 const (
 	defaultName = "big wide world"
@@ -19,9 +27,16 @@ const (
 var (
 	addr = flag.String("addr", "localhost:50051", "the address to connect to")
 	name = flag.String("name", defaultName, "Name to greet")
+	
+	p = Person{
+		Name:"Alice", 
+		Occupation: "Hairstylist", 
+		Age: "25",
+	}
 )
 
 func main() {
+	
 	flag.Parse()
 	// Set up a connection to the server.
 	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
@@ -35,8 +50,10 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: *name})
+	per, err := c.Person(ctx, &pb.PersonRequest{Name: p.Name, Occupation: p.Occupation, Age: p.Age})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
 	log.Printf("Subscribing service response: %s", r.GetMessage())
+	log.Printf("Subscribing service response: %s", per.GetMessage())
 }
